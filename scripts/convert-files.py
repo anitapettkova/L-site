757883b18bbe47796f1096495c1c1f9d5df0e28a
Lilko Petkov <lilko.petkovv@gmail.com>
2026-09-22 22:28:36 +0300

    feat: update content

---
+++ scripts/convert-files.py
@@ -0,0 +1,44 @@
+import os
+import markdown
+from os import walk
+from os.path import isfile, join
+
+TEMPLATE = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog Post</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="app-shell">
+        <main class="container">
+            <article class="markdown-body">
+                {content}
+            </article>
+        </main>
+    </div>
+</body>
+</html>"""
+
+
+def convert_files(path: str) -> None:
+    with open(path, "r") as f:
+        md_text = f.read()
+
+    html_content = markdown.markdown(md_text, extensions=["fenced_code", "tables"])
+
+    with open(path.replace("md", "html"), "w") as f:
+        f.write(TEMPLATE.format(content=html_content))
+
+
+def main() -> None:
+    for dirpath, _, filenames in walk("."):
+        for f in filenames:
+            if f.endswith(".md"):
+                concat_path = os.path.join(dirpath, f)
+                convert_files(concat_path)
+
+
+if __name__ == "__main__":
+    main()
